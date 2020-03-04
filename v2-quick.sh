9c3c6fa18cb48b4735d799601d7d5ac47b8b7e3c
YEYI <327979579@qq.com>
2020-03-04 19:45:36 +0800

    Update functional script

--- v2-quick.sh
+++ v2-quick.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # install sshpass
+sudo apt-get update
 sudo apt-get install sshpass
 
 ## detect sender server or receive server
@@ -12,14 +13,14 @@ else
 	echo "请输入参数; -s表示脚本运行在旧服务器上，-r表示脚本运行在新服务器上"
 fi
 
-## get IP and passwd
+## code for sender server
+
+# get IP and passwd
 if [[ ${sender_server} == 0 ]]; then
 	sender_ip=$2
 	sender_passwd=$3
 fi
 
-## code for sender server
-	
 # get IP and passwd
 if [[ ${sender_server} == 0 ]]; then
 	if [[ $2 == "" ]]||[[ $3 == "" ]]; then
@@ -36,11 +37,48 @@ if [[ ${sender_server} == 0 ]]; then
 	fi
 fi
 
-# Install v2-ui
-# echo "installing v2-ui"
-# `sudo chmod +x ./v2-ui.sh`
-# `sudo bash ./v2-ui.sh`
-# echo "installed v2-ui"
+## code for receiver server
+
+if [[ ${receiver_server} == 0 ]]; then
+	
+	# install v2-ui
+	sudo curl -Ls https://blog.sprov.xyz/v2-ui.sh | bash
+	
+	# change domain IP address
+	domain="" #your domain name
+	type_="A" 
+	key="" #godaddy api key
+	secret="" #godaddy api secret
+	name="" #the name of record
+	 
+	headers="Authorization: sso-key $key:$secret"
+	echo $headers
+	
+	result=$(curl -s -X GET -H "$headers" \
+	"https://api.godaddy.com/v1/domains/$domain/records/$type_/$name")
+
+	dnsIP=$(echo $result | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+	echo "dnsIP:" $dnsIP
+
+	# Get public ip address
+	ret=$(curl -s GET "http://ipinfo.io/json")
+	currentIP=$(echo $ret | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+	echo "currentIP:" $currentIP
+
+	curl -S -X PUT "https://api.godaddy.com/v1/domains/$domain/records/$type_/$name" \
+	-H "accept: application/json" \
+	-H "Content-Type: application/json" \
+	-H "$headers" \
+	-d "[ {\"data\": \"$currentIP\"} ]"
+
+	# install Let's Encrypt and certification files
+	sudo apt-get -y install socat
+	sudo mkdir /etc/v2ray
+	sudo curl https://get.acme.sh | sh
+	sudo ~/.acme.sh/acme.sh --issue -d domainName --standalone -k ec-256
+	sudo ~/.acme.sh/acme.sh --installcert -d domainName --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+	
+fi
 
 ## debug
 if [[ $sender_server == 0 ]]; then
